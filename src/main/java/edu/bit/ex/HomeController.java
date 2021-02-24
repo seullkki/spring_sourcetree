683396318ebaf9536a6f35827c3b61c21b5de129
seullkki <seullkki@gmail.com>
2021-02-24 16:35:27 +0900

    Revert "master #3"
    This reverts commit 2db741cfdf1bd3f02d9cb36e21947ffdb11b0bf0.

--- src/main/java/edu/bit/ex/HomeController.java
+++ src/main/java/edu/bit/ex/HomeController.java
@@ -11,7 +11,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-//master #3
 //master #2
 @Controller
 public class HomeController {
