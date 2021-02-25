3a88c6731dedc14fa49790a7bcceed4b65f14916
seullkki <seullkki@gmail.com>
2021-02-25 11:17:37 +0900

    no message

--- src/main/java/edu/bit/ex/BoardController.java
+++ src/main/java/edu/bit/ex/BoardController.java
@@ -11,7 +11,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-//master #7
 //master #6
 //master #5
 //master #4
